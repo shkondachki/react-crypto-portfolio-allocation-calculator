c43e8889962c0fccc635b41fc0979325b38a4a6d
Tsvetomir Shkondachki <cvetomir.shk@gmail.com>
2025-07-19 01:53:14 +0300

    Removed unused declartion

--- src/utils/cryptoIcons.tsx
+++ src/utils/cryptoIcons.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import * as Web3Icons from '@web3icons/react';
 
 export function getCryptoIcon(symbol: string, size = 22): ReactNode {
